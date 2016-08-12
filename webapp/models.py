6687ece7d1c562588ba30105107a96a37eeb483b
Doug Alexander <dougintexas@gmail.com>
2016-08-12 09:40:41 -0500

    Migrations to add new columns to DB

--- webapp/models.py
+++ webapp/models.py
@@ -19,7 +19,6 @@ CHOICES_CONFIRM = (
     ('annual screening', 'I should return once a year for annual screening.'),
 )
 
-
 class Survey(models.Model):
     survey_code = models.CharField(
         blank=True,
@@ -41,7 +40,7 @@ class Survey(models.Model):
         choices=CHOICES_CONCERNED,
         verbose_name='having a false alarm?'
     )
-    q_concerned_radiation = models.CharField(
+    q_concerned_rad = models.CharField(
         blank=True,
         null=True,
         max_length=50,
@@ -55,7 +54,7 @@ class Survey(models.Model):
         choices=CHOICES_CONCERNED,
         verbose_name='being treated for lung cancer that never would have harmed you?'
     )
-    q_concerned_surgery = models.CharField(
+    q_concerned_surg = models.CharField(
         blank=True,
         null=True,
         max_length=50,
@@ -83,7 +82,7 @@ class Survey(models.Model):
         choices=CHOICES_TALK,
         verbose_name='what happens if the lung cancer screening shows something that may need more testing.'
     )
-    q_talk_having_surgery = models.CharField(
+    q_talk_having_surg = models.CharField(
         blank=True,
         null=True,
         max_length=50,
@@ -101,6 +100,145 @@ class Survey(models.Model):
         max_length=50,
     )
 
+    q_talk_more_screening = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+        choices=CHOICES_TALK,
+        verbose_name='what happens if the lung cancer screening shows something that may need more testing.'
+    )
+    
+    d_harm_falsealarm_ment = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_falsealarm_disc = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_rad_ment = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_rad_disc = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_overdx_ment = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_overdx_disc = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_surg_ment = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_harm_surg_disc = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_focused_findcancer = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_focused_falsealarm = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_focused_rad = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_focused_overdx = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_focused_surg = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_talkabout_notscreening = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_talkabout_knowresults = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )
+
+    d_talkabout_moretesting = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_sure_clearaboutbandh = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_sure_knowaboutbandh = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_sure_sureaboutbestchoice = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_sure_havesupport = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_sure_bescreenedtoday = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )    
+
+    d_keymsg_quitsmoking = models.CharField(
+        blank=True,
+        null=True,
+        max_length=50,
+    )        
 
 class SurveyForm(ModelForm):
     class Meta:
@@ -109,15 +247,37 @@ class SurveyForm(ModelForm):
             'survey_code',
             'q_concerned_find_cancer',
             'q_concerned_false_alarm',
-            'q_concerned_radiation',
+            'q_concerned_rad',
             'q_concerned_unnecessary_treatment',
-            'q_concerned_surgery',
+            'q_concerned_surg',
             'q_talk_not_screened',
             'q_talk_how_soon',
             'q_talk_more_screening',
-            'q_talk_having_surgery',
+            'q_talk_having_surg',
             'q_confirm_quit_smoking',
             'q_confirm_annual_screening',
+            'd_harm_falsealarm_ment',
+            'd_harm_falsealarm_disc',
+            'd_harm_rad_ment',
+            'd_harm_rad_disc',
+            'd_harm_overdx_ment',
+            'd_harm_overdx_disc',
+            'd_harm_surg_ment',
+            'd_harm_surg_disc',
+            'd_focused_findcancer',
+            'd_focused_falsealarm',
+            'd_focused_rad',
+            'd_focused_overdx',
+            'd_focused_surg',
+            'd_talkabout_notscreening',
+            'd_talkabout_knowresults',
+            'd_talkabout_moretesting',
+            'd_sure_clearaboutbandh',
+            'd_sure_knowaboutbandh',
+            'd_sure_sureaboutbestchoice',
+            'd_sure_havesupport',
+            'd_sure_bescreenedtoday',
+            'd_keymsg_quitsmoking',
         ]
         widgets = {
             'survey_code': HiddenInput(),
