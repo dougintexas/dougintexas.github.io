a9c8816e74383860ba5b43430eabac72b9b9635d
Tony Adams <aadams@bcm.edu>
2016-08-12 12:09:23 -0500

    Coaching form submission and thanks page

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^coaching/(?P<survey_code>.+)/$', views.coaching),
     url(r'^thanks', views.thanks),
+    url(r'^final-thanks', views.coaching_thanks),
     url(r'^survey', views.index, name='index'),
     url(r'^start', views.start),
     url(r'^$', views.start, name='index'),
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import RequestContext
-from webapp.models import Survey, SurveyForm
+from webapp.models import Survey, SurveyForm, CoachingForm
 
 
 def index(request):
@@ -16,7 +18,6 @@ def start(request):
 
 
 def thanks(request):
-    print(request)
     if request.method == 'POST':
         submission = SurveyForm(request.POST)
         submission.save()
@@ -24,7 +25,17 @@ def thanks(request):
     return redirect('start')
 
 
-def coaching(request, survey_code):
-    survey = get_object_or_404(Survey, survey_code=survey_code)
+def coaching_thanks(request):
+    return render(request, 'coach_thanks.html')
 
+
+def coaching(request, survey_code):
+    if request.method == 'POST':
+        user_survey = Survey.objects.get(survey_code=survey_code)
+        form = CoachingForm(request.POST, instance=user_survey)
+        form.save()
+        return HttpResponseRedirect('/final-thanks')
+
+    survey = get_object_or_404(Survey, survey_code=survey_code)
+    print(survey.survey_code)
     return render(request, 'dec_coaching.html', {'survey': survey})
